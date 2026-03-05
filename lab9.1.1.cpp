05ab7a035d13332848fb2ba02d61714c48474be8
6604062636402 <s6604062636402@email.kmutnb.ac.th>
2026-03-05 14:38:31 +0700

    Add files via upload

---
+++ lab9.1.1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void swapValue(int *a, int *b, int *c);
+void swapArray(int a[], int b[], int size);
+
+int main() {
+    int a = 1, b = 2, c = 3;
+
+    printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
+
+    swapValue(&a, &b, &c);
+
+    printf("After swap function: a=%d, b=%d, c=%d\n", a, b, c);
+
+    return 0;
+}
+
+
+void swapValue(int *a, int *b, int *c) {
+    int tempA = *a;
+    int tempB = *b;
+    int tempC = *c;
+
+    *a = tempB;  
+    *b = tempC;  
+    *c = tempA;  
+}
